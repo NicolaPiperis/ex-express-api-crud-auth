a6ef2ba4b80db63571114a20947c4cab379d6aeb
NicolaPiperis <nicolapiperis9999@gmail.com>
2023-11-27 15:59:34 +0100

    completed

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,10 +3,42 @@ const prisma = new PrismaClient();
 const { matchedData } = require("express-validator");
 const bcrypt = require("bcrypt");
 const jsonwebtoken = require("jsonwebtoken");
+const AuthError = require("../exceptions/AuthError");
 
 
 async function login (req, res) {
+      // Recuperare i dati inseriti dall'utente
+  const { email, password } = req.body;
 
+  // controllare che ci sia un utente con quella email
+  const user = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  });
+
+  if (!user) {
+    return next(new AuthError("Utente non trovato"));
+  }
+
+  // controllare che la password sia corretta
+  const passMatch = await bcrypt.compare(password, user.password);
+
+  if (!passMatch) {
+    return next(new AuthError("Password errata"));
+  }
+
+  // generare il token JWT
+  const token = jsonwebtoken.sign(user, process.env.JWT_SECRET, {
+    expiresIn: "1h",
+  });
+
+  // ritornare il token ed i dati dell'utente
+
+  // rimuovo la password dall'oggetto user
+  delete user.password;
+
+  res.json({ user, token });
 }
 
 async function register (req, res) {
--- routers/auth.js
+++ routers/auth.js
@@ -4,8 +4,9 @@ const authController = require("../controllers/auth");
 const { checkSchema } = require("express-validator");
 const {checkValidity} = require("../middleware/schemaValidator");
 const userRegister = require("../validations/userRegister");
+const userLogin = require("../validations/userLogin");
 
-router.post("/login", authController.login);
+router.post("/login", checkSchema(userLogin), checkValidity, authController.login);
 router.post("/register", checkSchema(userRegister), checkValidity, authController.register);
 
 module.exports = router;
